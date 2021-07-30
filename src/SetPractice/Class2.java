02d4a884b3b3a83a2d576d7f3742c648e6c62a54
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-07-30 09:17:11 -0500

    new piece of the code added

--- src/SetPractice/Class2.java
+++ src/SetPractice/Class2.java
@@ -3,7 +3,9 @@ package SetPractice;
 public class Class2 {
     public static void main(String[] args) {
         String str=" What is SDLC?";
-
+        if (str.contains("SDLC")){
+            System.out.println("Software Development Life Cycle");
+        }
 
 
     }
