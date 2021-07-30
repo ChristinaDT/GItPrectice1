884e5b9e12af6487a2a91234a649b63e2a485ae5
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-07-30 09:31:03 -0500

    added new script conflict

--- src/Main/Car.java
+++ src/Main/Car.java
@@ -4,6 +4,7 @@ public class Car {
     public static void main(String[] args) {
         // SDLC== Software Development Life Cycle
         // A process that is used in software industry to design,
-        //develop and test a high quality
+        //develop and test a high quality software that has the purpose
+        //to exceed the customer expectations
     }
 }
