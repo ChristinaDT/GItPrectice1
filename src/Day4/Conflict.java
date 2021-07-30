64b16af1ebaa95110a3c6ece9543da76300798d6
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-07-30 09:45:15 -0500

    file created

--- src/Day4/Conflict.java
+++ src/Day4/Conflict.java
@@ -3,5 +3,6 @@ package Day4;
 public class Conflict {
     public static void main(String[] args) {
 //`This is collection branch content`
+        System.out.println("Thanks God It's Friday!");
     }
 }
