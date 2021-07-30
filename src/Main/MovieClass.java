7456b6b53ba9fd7ff73e0d9255418b83b6fffb31
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-07-30 09:23:34 -0500

    new stuff added

--- src/Main/MovieClass.java
+++ src/Main/MovieClass.java
@@ -4,6 +4,7 @@ package Main;
 
 public class MovieClass {
     public static void main(String[] args) {
-        System.out.println("Movie starts in 5");
+        System.out.println("Movie starts in 5 min");
+        System.out.println("Movie at 10 min");
     }
 }
