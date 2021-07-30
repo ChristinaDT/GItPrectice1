8b9290968adcea75b080e96a024f1f5480b76878
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-07-30 09:49:33 -0500

    collection2 added in Conflict

--- src/Day4/Conflict.java
+++ src/Day4/Conflict.java
@@ -4,5 +4,7 @@ public class Conflict {
     public static void main(String[] args) {
 //`This is collection branch content`
         System.out.println("Thanks God It's Friday!");
+        //more work on collection2
+        String str=" Creating conflicts";
     }
 }
