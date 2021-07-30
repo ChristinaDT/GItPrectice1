c00c8cec886f4701dd0345894b78e514e33b2938
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-07-30 10:33:40 -0500

    here we go again

--- src/Day4/TC002.java
+++ src/Day4/TC002.java
@@ -3,5 +3,6 @@ package Day4;
 public class TC002 {
     public static void main(String[] args) {
         System.out.println("Start the day with a smile");
+        System.out.println("Here we go again");
     }
 }
