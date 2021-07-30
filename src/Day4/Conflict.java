5acb7c2e18c95d6507e3a0d7d10db4b7e5926d32
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-07-30 09:50:45 -0500

    master added in Conflict

--- src/Day4/Conflict.java
+++ src/Day4/Conflict.java
@@ -4,5 +4,8 @@ public class Conflict {
     public static void main(String[] args) {
 //`This is collection branch content`
         System.out.println("Thanks God It's Friday!");
+
+        int num1= 25;
+        int num2=30;
     }
 }
