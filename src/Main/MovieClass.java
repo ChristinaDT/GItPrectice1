7203f15fde22a946af76617b65da9a04fd767014
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-07-30 09:26:00 -0500

    added new class conflict

--- src/Main/MovieClass.java
+++ src/Main/MovieClass.java
@@ -3,7 +3,7 @@ package Main;
 public class MovieClass {
     public static void main(String[] args) {
         //Let's create a fun script
-
+        System.out.println("Generating the conflict");
     }
 
 }
