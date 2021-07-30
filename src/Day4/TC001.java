d666851fed2bb9aa9c354e8829e50bb38af203e8
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-07-30 10:28:59 -0500

    Stay motivated

--- src/Day4/TC001.java
+++ src/Day4/TC001.java
@@ -4,5 +4,6 @@ public class TC001 {
     public static void main(String[] args) {
         System.out.println("Be consistent");
         System.out.println("Be brave");
+        System.out.println("Stay motivated");
     }
 }
